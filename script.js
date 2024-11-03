b3cb275b5e13a0204cc2851fa7f8e41078d3a653
Vamshi Reddy <106398124+mv735065@users.noreply.github.com>
2024-11-03 19:44:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+
+let ele=document.getElementById("level");
+
+
+let value=getDom(ele);
+
+alert(value);
+
+
+function getDom(ele) {
+	if(ele==null) return 0;
+	
+	return 1+getDom(ele.parentElement);
+}
